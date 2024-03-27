5577011376659fef93adaea94ac2d42dee6c6d53
DysonCarter <129555170+DysonCarter@users.noreply.github.com>
2024-03-27 14:45:03 -0400

    Add frame rate

--- traffic.py
+++ traffic.py
@@ -9,6 +9,7 @@ pygame.init()
 width = 1000
 height = 500
 screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
@@ -16,3 +17,4 @@ while True:
             pygame.quit()
             exit()
     pygame.display.update()
+    clock.tick(60);
