1c9907d9785f58319668e6ab6cca7056e793712b
DysonCarter <129555170+DysonCarter@users.noreply.github.com>
2024-03-27 14:15:06 -0400

    Make window usable

--- traffic.py
+++ traffic.py
@@ -1,3 +1,16 @@
 # traffic.py by Dyson Carter
 
 import pygame
+
+pygame.init()
+
+# Make Window
+width = 1000
+height = 500
+screen = pygame.display.set_mode((width, height))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    pygame.display.update()
