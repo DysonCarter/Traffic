bd1cc8ca1d8e7839adbe5e5b45b8ac250852dc9d
DysonCarter <129555170+DysonCarter@users.noreply.github.com>
2024-03-29 17:33:54 -0400

    Add new strategy (BUGGED)

--- traffic.py
+++ traffic.py
@@ -19,8 +19,9 @@ lane_count = 2
 lane1_height = height * .15
 lane2_height = height * .50
 lane3_height = height * .85
+lane_height = [height * .15, height * .5, height * .85]
 car_radius = 10
-car_count = 10
+car_count = 5
 
 # Create window
 screen = pygame.display.set_mode((width, height))
@@ -40,10 +41,13 @@ class Car:
             self.y = lane3_height
             
         self.lane = lane
-        self.x = random.randint(0, int(width * 0.75))
+        self.x = random.randint(0, int(width * 1))
         self.initial_speed = random.uniform(1, 3)  # Random initial speed
         self.speed = self.initial_speed
         self.strategy = strategy()
+        self.lane_change_delay = 30  # Delay in frames before another lane change is allowed
+        self.lane_change_timer = 0    # Timer to track the delay
+        self.lane_change_step = 1     # Step size for lane change
 
     def move(self):
         # Move horizontally
@@ -60,52 +64,83 @@ class Car:
             return False
         if self.lane != other_car.lane:
             return False
-        if not (0 <= other_car.x - self.x <= 30):
-            return False
-        return True
+
+        # Calculate distance considering looping
+        if self.x <= other_car.x:
+            distance = other_car.x - self.x
+        else:
+            distance = width - self.x + other_car.x
+
+        if distance <= 100:
+            return True
+        return False
+
     
-    # Checks to see if left lane change can commence
+    # Checks to see if lane change can commence
     # Returns true if it is ok, returns false if the other car is there
     def check_left_mirror(self, other_car):
         if other_car.lane != (self.lane - 1):
             return True
-        if (self.x - 150 <= other_car.x <= self.x + 150):
+
+        # Calculate distance considering screen looping
+        if self.x <= other_car.x:
+            distance_x = other_car.x - self.x
+        else:
+            distance_x = width - self.x + other_car.x
+
+        if self.y <= other_car.y:
+            distance_y = other_car.y - self.y
+        else:
+            distance_y = height - self.y + other_car.y
+
+        if distance_x <= 100 and distance_y <= 100:
+            return False
+
+        return True
+
+    def check_right_mirror(self, other_car):
+        if other_car.lane != (self.lane + 1):
             return True
-        if (other_car.x >= self.x + 30) and (other_car.speed >= self.speed):
-            return True
-        if (other_car.x <= self.x - 30) and (other_car.speed <= self.speed):
-            return True
-        return False
+
+        # Calculate distance considering screen looping
+        if self.x <= other_car.x:
+            distance_x = other_car.x - self.x
+        else:
+            distance_x = width - self.x + other_car.x
+
+        if self.y <= other_car.y:
+            distance_y = other_car.y - self.y
+        else:
+            distance_y = height - self.y + other_car.y
+
+        if distance_x <= 100 and distance_y <= 100:
+            return False
+
+        return True
     
     def try_lane_change_left(self):
-        if self.lane == 1:
+        if self.lane == 1 or self.lane_change_timer > 0:
             return
-        if self.lane == 2:
-            if self.y != lane1_height:
-                self.y -= 1
-            else:
-                self.lane = 1
-        if self.lane == 3: 
-            if self.y != lane2_height:
-                self.y -= 1
-            else:
-                self.lane = 2
-        return
+        target_lane = self.lane - 1
+        if self.y > lane_height[target_lane - 1]:
+            self.y -= self.lane_change_step
+        else:
+            self.lane -= 1
+            self.lane_change_timer = self.lane_change_delay  # Start the delay timer
 
     def try_lane_change_right(self):
-        if self.lane == 3:
+        if self.lane == 3 or self.lane_change_timer > 0:
             return
-        if self.lane == 2:
-            if self.y != lane3_height:
-                self.y += 1
-            else:
-                self.lane = 3
-        if self.lane == 1: 
-            if self.y != lane2_height:
-                self.y += 1
-            else:
-                self.lane = 2
-        return
+        target_lane = self.lane + 1
+        if self.y < lane_height[target_lane - 1]:
+            self.y += self.lane_change_step
+        else:
+            self.lane += 1
+            self.lane_change_timer = self.lane_change_delay  # Start the delay timer
+
+    def update_lane_change_timer(self):
+        if self.lane_change_timer > 0:
+            self.lane_change_timer -= 1
 
 # Basic Strategy Class
 # Grin and Bare it Strategy - No Changing Lanes
@@ -114,6 +149,26 @@ class BasicStrategy:
         for other_car in cars:
             if car.will_collide(other_car) and (car != other_car):
                 car.speed = other_car.speed
+                break
+
+class NiceStrategy:
+    def run_strategy(self, car, cars):
+        right_lane_good = True
+        left_lane_good = True
+        for other_car in cars:
+            if not car.check_right_mirror(other_car):
+                right_lane_good = False
+            if not car.check_left_mirror(other_car):
+                left_lane_good = False
+        if right_lane_good:
+            car.try_lane_change_right()
+
+        for other_car in cars:
+            if car.will_collide(other_car) and (car != other_car):
+                if left_lane_good:
+                    car.try_lane_change_left()
+                else:
+                    car.speed = other_car.speed
                 break
 
 # Function to draw lanes
@@ -126,7 +181,7 @@ def draw_lanes():
         pygame.draw.rect(screen, WHITE, (lane_x, lane_y, width, lane_height))
 
 # Create all cars
-cars = [Car(random.randint(1, lane_count), BasicStrategy) for _ in range(car_count)]
+cars = [Car(random.randint(1, lane_count), NiceStrategy) for _ in range(car_count)]
 
 # Main loop
 running = True
@@ -143,6 +198,7 @@ while running:
         car.move()
         car.draw()
         car.strategy.run_strategy(car, cars)
+        car.update_lane_change_timer()
 
     pygame.display.update()
     clock.tick(60)
